f754972980f3bab1a8df366c36a0d834d8e92f30
ZahirAndres <113659010+ZahirAndres@users.noreply.github.com>
2024-04-20 00:28:45 -0600

    Update IPrincipalDao.java

--- edson/edson/src/main/java/mx/utng/edson/model/dao/IPrincipalDao.java
+++ edson/edson/src/main/java/mx/utng/edson/model/dao/IPrincipalDao.java
@@ -2,11 +2,7 @@ package mx.utng.edson.model.dao;
 import java.util.List;
 
 import mx.utng.edson.model.entity.Principal;
-/**
- * @autor Zahir Andres Rodriguez Mora
- * @category GDS0622
- * @size 17/04/2024
- */
+
 public interface IPrincipalDao {
 
   List<Principal> list();
