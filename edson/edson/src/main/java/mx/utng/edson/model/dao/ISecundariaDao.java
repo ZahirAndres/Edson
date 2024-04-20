9aa38621b90741d7b5176a0e1e59ac82e4a6d441
ZahirAndres <113659010+ZahirAndres@users.noreply.github.com>
2024-04-20 00:29:06 -0600

    Update ISecundariaDao.java

--- edson/edson/src/main/java/mx/utng/edson/model/dao/ISecundariaDao.java
+++ edson/edson/src/main/java/mx/utng/edson/model/dao/ISecundariaDao.java
@@ -2,11 +2,7 @@ package mx.utng.edson.model.dao;
 import java.util.List;
 
 import mx.utng.edson.model.entity.Secundaria;
-/**
- * @autor Zahir Andres Rodriguez Mora
- * @category GDS0622
- * @size 17/04/2024
- */
+
 public interface ISecundariaDao {
     List<Secundaria> list();
   void save(Secundaria secundaria);
